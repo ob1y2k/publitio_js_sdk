1e829fa36a14eb83070dc3f8878aa269247f192a
Enn Michael <enntheprogrammer@gmail.com>
2019-10-25 21:14:52 +0200

    Hopefully I didn't break anything

--- demo/temp-example.js
+++ demo/temp-example.js
@@ -3,6 +3,6 @@ const fs = require('fs')
 
 const file = fs.readFileSync('/home/mogwai/music.bak/Music/car seat headrest - nervous young man (2013)/01 boxing day.mp3')
 const publitio = publitioApi('NsSyzZGG4NDkGbbkc44g', '6BWw4SalTm6qDnl6mH7gv54RtT9hpVuT')
-publitio.uploadFile(file, 'file', { title: 'nervous' })
+publitio.call('/files/update/G5DeRBJX', 'PUT', { description: 'my left tit' })
   .then(r => console.log(r))
   .catch(e => console.log(e))
